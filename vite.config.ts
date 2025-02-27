0e8e3d9b880e0f295549d080c77da3118603a514
Farid Rener <proteusvacuum@gmail.com>
2025-02-26 20:47:32 -0500

    more changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/phonemes/',  // Replace with your repository name
+  base: '/phonemes/',  // Subdirectory path
   optimizeDeps: {
     include: ['lucide-react'],
   },
